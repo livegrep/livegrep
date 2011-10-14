a5b9761f92a9020ec9e710960e9a43ff684075df
Nelson Elhage <nelhage@nelhage.com>
2011-10-14 18:53:21 -0400

    tree walking.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 libgit2=/home/nelhage/sw/libgit2
-CXXFLAGS=-I$(libgit2)/include
-LDFLAGS=-L$(libgit2)/lib -lgit2
+CXXFLAGS=-I$(libgit2)/include -ggdb3
+LDFLAGS=-L$(libgit2)/lib -lgit2 -Wl,-R$(libgit2)/lib
 
 all: wc
--- wc.cc
+++ wc.cc
@@ -1,11 +1,36 @@
 #include <stdio.h>
 
+extern "C" {
 #include <git2.h>
+};
+
+void lookup_head(git_repository *repo, const git_oid **oid) {
+    git_reference *ref;
+    git_reference_lookup(&ref, repo, "HEAD");
+    if (git_reference_type(ref) == GIT_REF_SYMBOLIC) {
+        const char *target = git_reference_target(ref);
+        git_reference_lookup(&ref, repo, target);
+    }
+    *oid = git_reference_oid(ref);
+}
+
+void walk_tree(git_tree *tree) {
+    
+}
 
 int main(int argc, char **argv) {
-
     git_repository *repo;
-    git_repository_open(&repo, ".");
+    git_repository_open(&repo, ".git");
+
+    const git_oid *oid;
+    lookup_head(repo, &oid);
+
+    git_commit *commit;
+    git_tree *tree;
+    git_commit_lookup(&commit, repo, oid);
+    git_commit_tree(&tree, commit);
+
+    walk_tree(tree);
 
     return 0;
 }
