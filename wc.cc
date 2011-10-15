84aa7d8d0a35a2ca5424e2792e153e07d770f694
Nelson Elhage <nelhage@nelhage.com>
2011-10-15 12:43:24 -0400

    Add a smart wrapper class around git_object and friends.
    This is probably too clever by half, but it's arguably useful.

--- wc.cc
+++ wc.cc
@@ -3,62 +3,114 @@
 
 #include <git2.h>
 
-class smart_object {
+class smart_object_base {
 public:
-    smart_object() : obj_(0) {
+    smart_object_base() : obj_(0) {
     };
 
     operator git_object** () {
         return &obj_;
     }
 
-    operator git_tree* () {
-        assert(git_object_type(obj_) == GIT_OBJ_TREE);
-        return reinterpret_cast<git_tree*>(obj_);
+    ~smart_object_base() {
+        if (obj_)
+            git_object_close(obj_);
     }
 
 protected:
+    smart_object_base(const smart_object_base& rhs) {
+    }
     git_object *obj_;
 };
 
-void lookup_head(git_repository *repo, const git_oid **oid) {
-    git_reference *ref;
-    git_reference_lookup(&ref, repo, "HEAD");
-    if (git_reference_type(ref) == GIT_REF_SYMBOLIC) {
-        const char *target = git_reference_target(ref);
-        git_reference_lookup(&ref, repo, target);
-    }
-    *oid = git_reference_oid(ref);
-}
+template <class T>
+class object_traits { const static git_otype type; };
 
-void walk_tree(git_repository *repo, git_tree *tree, int indent) {
-    int entries = git_tree_entrycount(tree);
-    int i;
-    for (i = 0; i < entries; i++) {
-        const git_tree_entry *ent = git_tree_entry_byindex(tree, i);
-        printf("%*s%s\n", indent, " ", git_tree_entry_name(ent));
-        if (git_tree_entry_type(ent) == GIT_OBJ_TREE) {
-            smart_object subtree;
-            git_tree_entry_2object(subtree, repo, ent);
-            walk_tree(repo, subtree, indent + 1);
+template <>
+struct object_traits<git_tree> { const static git_otype git_type = GIT_OBJ_TREE; };
+template <>
+struct object_traits<git_commit> { const static git_otype git_type = GIT_OBJ_COMMIT; };
+template <>
+struct object_traits<git_blob> { const static git_otype git_type = GIT_OBJ_BLOB; };
+
+template <class T>
+class smart_object : public smart_object_base {
+public:
+    operator T* () {
+        assert(obj_);
+        assert(git_object_type(obj_) == object_traits<T>::git_type);
+        return reinterpret_cast<T*>(obj_);
+    }
+    operator T** () {
+        assert(obj_ == 0);
+        return reinterpret_cast<T**>(&obj_);
+    }
+};
+
+template <>
+class smart_object <git_object> : public smart_object_base {
+public:
+    template <class O>
+    operator O* () {
+        assert(git_object_type(obj_) == object_traits<O>::git_type);
+        return reinterpret_cast<O*>(obj_);
+    }
+};
+
+class code_counter {
+public:
+    code_counter(git_repository *repo) : repo_(repo) {
+    }
+
+    void run() {
+        const git_oid *oid;
+        lookup_head(&oid);
+
+        smart_object<git_commit> commit;
+        smart_object<git_tree> tree;
+        git_commit_lookup(commit, repo_, oid);
+        git_commit_tree(tree, commit);
+
+        walk_tree(tree, 0);
+        git_tree_close(tree);
+    }
+protected:
+    void walk_tree(git_tree *tree, int indent) {
+        int entries = git_tree_entrycount(tree);
+        int i;
+        for (i = 0; i < entries; i++) {
+            const git_tree_entry *ent = git_tree_entry_byindex(tree, i);
+            smart_object<git_tree> obj;
+            git_tree_entry_2object(obj, repo_, ent);
+            printf("%*s%s\n", indent, "", git_tree_entry_name(ent));
+            if (git_tree_entry_type(ent) == GIT_OBJ_TREE) {
+                walk_tree(obj, indent + 1);
+            } else if (git_tree_entry_type(ent) == GIT_OBJ_BLOB) {
+
+            }
         }
     }
-}
+
+
+    void lookup_head(const git_oid **oid) {
+        git_reference *ref;
+        git_reference_lookup(&ref, repo_, "HEAD");
+        if (git_reference_type(ref) == GIT_REF_SYMBOLIC) {
+            const char *target = git_reference_target(ref);
+            git_reference_lookup(&ref, repo_, target);
+        }
+        *oid = git_reference_oid(ref);
+    }
+
+    git_repository *repo_;
+};
 
 int main(int argc, char **argv) {
     git_repository *repo;
     git_repository_open(&repo, ".git");
 
-    const git_oid *oid;
-    lookup_head(repo, &oid);
-
-    git_commit *commit;
-    git_tree *tree;
-    git_commit_lookup(&commit, repo, oid);
-    git_commit_tree(&tree, commit);
-
-    walk_tree(repo, tree, 0);
-    git_tree_close(tree);
+    code_counter counter(repo);
+    counter.run();
 
     return 0;
 }
