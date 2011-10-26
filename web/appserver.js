cb75d47f12fcb1bea4290b9aace44a784f4324c5
Nelson Elhage <nelhage@nelhage.com>
2011-10-26 12:42:13 -0700

    Refactor codesearch.js to have separate Server and Client classes.

--- web/appserver.js
+++ web/appserver.js
@@ -4,33 +4,55 @@ var REPO = '/home/nelhage/code/codesearch/'
 
 var searcher = new Codesearch(REPO);
 
-function Server(remote, conn) {
+var clients = {};
+
+function Client(remote) {
+  this.remote = remote;
   this.pending_search = null;
   this.last_search = null;
-  this.new_search = function(str) {
-    if (str === this.last_search)
-      return;
-    this.pending_search = str;
-    if (searcher.readyState == 'ready') {
-      this.dispatch_search();
-    }
-  }
-  searcher.on('ready', function () {
-                this.dispatch_search();
-              }.bind(this));
 }
 
-Server.prototype.dispatch_search = function() {
+Client.prototype.new_search = function (str) {
+  if (str === this.last_search)
+    return;
+  this.pending_search = str;
+  if (searcher.readyState == 'ready') {
+    this.dispatch_search();
+  }
+}
+
+Client.prototype.dispatch_search = function() {
   if (this.pending_search !== null) {
     this.last_search = this.pending_search;
     console.log('dispatching: %s...', this.pending_search)
     var search = searcher.search(this.pending_search);
     this.pending_search = null;
-    search.on('error', function () {});
+    search.on('error', function () {
+
+              });
     search.on('match', function (match) {
       console.log("[%s]: %j", search.search, match);
     });
   }
 }
+
+
+function Server(remote, conn) {
+  clients[conn.id] = new Client(remote);
+  this.new_search = function(str) {
+    clients[conn.id].new_search(str);
+  }
+  conn.on('end', function() {
+            delete clients[conn.id];
+          });
+}
+
+searcher.on('ready', function () {
+              Object.keys(clients).forEach(
+                function (id) {
+                  clients[id].dispatch_search();
+                });
+            });
+
 
 module.exports = Server;
