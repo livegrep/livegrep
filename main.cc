a70fd353fa2ca1351037eaef9291bafd2d748d51
Nelson Elhage <nelhage@nelhage.com>
2011-10-26 10:24:01 -0700

    Tweak the machine-accessible mode somewhat.

--- main.cc
+++ main.cc
@@ -28,15 +28,14 @@ int main(int argc, char **argv) {
             printf(" done in %d.%06ds\n",
                    (int)elapsed.tv_sec, (int)elapsed.tv_usec);
     }
-    if (FLAG_machine_interface)
-        printf("DONE\n");
-    else
+    if (!FLAG_machine_interface)
         counter.dump_stats();
     RE2::Options opts;
     opts.set_never_nl(true);
     opts.set_one_line(false);
     opts.set_perl_classes(true);
     opts.set_posix_syntax(true);
+    opts.set_log_errors(false);
     while (true) {
         if (FLAG_machine_interface)
             printf("READY\n");
@@ -47,6 +46,12 @@ int main(int argc, char **argv) {
         if (cin.eof())
             break;
         RE2 re(line, opts);
+        if (!re.ok()) {
+            if (!FLAG_machine_interface)
+                printf("Error: %s\n", re.error().c_str());
+            else
+                printf("FATAL %s\n", re.error().c_str());
+        }
         if (re.ok()) {
             timer tm;
             struct timeval elapsed;
