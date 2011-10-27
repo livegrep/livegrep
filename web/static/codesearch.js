d874c39210efb9abf0cd36ddd6c88b5b5442ab1c
Nelson Elhage <nelhage@nelhage.com>
2011-10-26 18:02:59 -0700

    Add reconnection to the web UI.
    Apparently {reconnect: timeout} just doesn't work at all in the
    browser with dnode. Furthermore, socket.io gives you absolutely no way
    to check for a failed connection.

--- web/static/codesearch.js
+++ web/static/codesearch.js
@@ -1,19 +1,41 @@
 "use strict";
 var Codesearch = function() {
+  var MAX_RECONNECT_INTERVAL = 1000*60*1;
   return {
     remote: null,
     displaying: null,
+    reconnect_interval: 50,
     onload: function() {
       Codesearch.input = $('#searchbox');
       Codesearch.input.keydown(Codesearch.keypress);
+      Codesearch.connect()
+    },
+    connect: function() {
+      if (Codesearch.remote !== null)
+        return;
+      console.log("Connecting...");
+      Codesearch.remote = null;
+      var timer = setTimeout(Codesearch.connect_failed, 500);
       DNode({ error: Codesearch.regex_error,
               match: Codesearch.match,
               search_done: Codesearch.search_done,
-            }).connect(function (remote) {
-                      Codesearch.remote = remote;
-                    }, {
-                      reconnect: 100
+            }).connect(function (remote, conn) {
+                         Codesearch.remote = remote;
+                         conn.on('end', Codesearch.disconnected);
+                         clearTimeout(timer);
+                         Codesearch.reconnect_interval = 50;
                     });
+    },
+    connect_failed: function() {
+      Codesearch.disconnected();
+    },
+    disconnected: function() {
+      console.log("Reconnecting in " + Codesearch.reconnect_interval)
+      Codesearch.remote = null;
+      setTimeout(Codesearch.connect, Codesearch.reconnect_interval);
+      Codesearch.reconnect_interval *= 2;
+      if (Codesearch.reconnect_interval > MAX_RECONNECT_INTERVAL)
+        Codesearch.reconnect_interval = MAX_RECONNECT_INTERVAL;
     },
     keypress: function() {
       setTimeout(Codesearch.newsearch, 0);
