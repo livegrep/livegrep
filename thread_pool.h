5ab3980649e7a921a453f27bcdaf986f8fb23f00
Nelson Elhage <nelhage@nelhage.com>
2011-11-11 20:53:28 -0500

    Remove the subclass and virtual function-call tricks from thread_pool.

--- thread_pool.h
+++ thread_pool.h
@@ -5,19 +5,32 @@
 
 #include "thread_queue.h"
 
-template <class J, class DoIt>
-class base_thread_pool {
+struct no_thread_state;
+
+template <class J, class DoIt, class ThreadState = no_thread_state>
+class thread_pool;
+
+template<class J, class DoIt, class ThreadState>
+void _thread_worker(thread_pool<J, DoIt, ThreadState>&);
+template<class J, class DoIt>
+void _thread_worker(thread_pool<J, DoIt, no_thread_state>&);
+
+template <class J, class DoIt, class ThreadState>
+class thread_pool {
 public:
-    base_thread_pool (int nthreads, DoIt& fn)
+    thread_pool (int nthreads, DoIt& fn)
         : nthreads_(nthreads), fn_(fn) {
         threads_ = new pthread_t[nthreads_];
+        for (int i = 0; i < nthreads_; i++) {
+            pthread_create(&threads_[i], NULL, worker, this);
+        }
     }
 
     void queue(const J& job) {
         queue_.push(job);
     }
 
-    virtual ~base_thread_pool () {
+    ~thread_pool () {
         int i;
         for (i = 0; i < nthreads_; i++) {
             pthread_join(threads_[i], NULL);
@@ -31,69 +44,39 @@ protected:
     thread_queue<J> queue_;
     DoIt& fn_;
 
-    virtual void worker()=0;
-
     static void *worker(void *arg) {
-        base_thread_pool *pool = static_cast<base_thread_pool*>(arg);
-        pool->worker();
+        thread_pool *pool = static_cast<thread_pool*>(arg);
+        _thread_worker(*pool);
         return NULL;
     }
 
-    void start_threads() {
-        for (int i = 0; i < nthreads_; i++) {
-            pthread_create(&threads_[i], NULL, worker, this);
-        }
-    }
+    friend void _thread_worker<>(thread_pool&);
+    friend void _thread_worker<>(thread_pool<J, DoIt, no_thread_state>&);
 };
+
 
 struct no_thread_state {};
 
-template<class J, class DoIt, class ThreadState = no_thread_state>
-class thread_pool;
-
 template<class J, class DoIt, class ThreadState>
-class thread_pool : public base_thread_pool <J, DoIt> {
-public:
-    thread_pool(int nthreads, DoIt& doit)
-        : base_thread_pool<J, DoIt>(nthreads, doit) {
-        this->start_threads();
+void _thread_worker(thread_pool<J, DoIt, ThreadState>& pool)
+{
+    ThreadState ts(pool.fn_);
+    while (true) {
+        J job = pool.queue_.pop();
+        if (pool.fn_(ts, job))
+            break;
     }
-
-    virtual ~thread_pool() {}
-
-protected:
-    virtual void worker()
-    {
-        ThreadState ts(this->fn_);
-        while (true) {
-            J job = this->queue_.pop();
-            if (this->fn_(ts, job))
-                break;
-        }
-    }
-};
+}
 
 template <class J, class DoIt>
-class thread_pool<J, DoIt, no_thread_state> :
-    public base_thread_pool <J, DoIt> {
-public:
-    thread_pool(int nthreads, DoIt& doit)
-        : base_thread_pool<J, DoIt>(nthreads, doit) {
-        this->start_threads();
+void _thread_worker(thread_pool<J, DoIt, no_thread_state>& pool)
+{
+    while (true) {
+        J job = pool.queue_.pop();
+        if (pool.fn_(job))
+            break;
     }
-
-    virtual ~thread_pool() {}
-
-protected:
-    virtual void worker()
-    {
-        while (true) {
-            J job = this->queue_.pop();
-            if (fn_(job))
-                break;
-        }
-    }
-};
+}
 
 #endif /* CODESEARCH_THREAD_POOL_H */
 
