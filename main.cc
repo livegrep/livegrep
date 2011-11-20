a6cf767471d27bfb649adefa73c741f8a46571a0
Nelson Elhage <nelhage@nelhage.com>
2011-11-20 18:53:03 -0500

    Allow word-boundary syntax.

--- main.cc
+++ main.cc
@@ -81,6 +81,7 @@ int main(int argc, char **argv) {
     opts.set_never_nl(true);
     opts.set_one_line(false);
     opts.set_perl_classes(true);
+    opts.set_word_boundary(true);
     opts.set_posix_syntax(true);
     opts.set_log_errors(false);
     while (true) {
