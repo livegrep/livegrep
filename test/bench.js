ed919c5f2350d17c6b4b4e370c1dfa840e240107
Nelson Elhage <nelhage@nelhage.com>
2011-12-26 16:52:04 -0500

    bench.js: Reindent with two-space indent.

--- test/bench.js
+++ test/bench.js
@@ -14,87 +14,87 @@ var times = { };
 var ITERATIONS = options.iterations;
 
 function loop(i) {
-    if (i == queries.length) {
-        if (ITERATIONS == 0) {
-            done();
-        } else {
-            ITERATIONS--;
-            loop(0);
-        }
-        return;
+  if (i == queries.length) {
+    if (ITERATIONS == 0) {
+      done();
+    } else {
+      ITERATIONS--;
+      loop(0);
     }
-    var q = queries[i];
-    var start = new Date();
-    var search = cs.search(q);
-    var results = 0;
-    search.on('match', function () {
-                  results++;
-              })
-    search.on('done',
-              function (stats) {
-                  var end = new Date();
-                  var time = +(end - start);
-                  if (!(q in times))
-                      times[q] = [];
-                  stats.time = time;
-                  stats.nmatch = results;
-                  times[q].push(stats);
-                  cs.once('ready', function() {
-                              loop(i+1);
-                          });
-              });
+    return;
+  }
+  var q = queries[i];
+  var start = new Date();
+  var search = cs.search(q);
+  var results = 0;
+  search.on('match', function () {
+              results++;
+            })
+  search.on('done',
+            function (stats) {
+              var end = new Date();
+              var time = +(end - start);
+              if (!(q in times))
+                times[q] = [];
+              stats.time = time;
+              stats.nmatch = results;
+              times[q].push(stats);
+              cs.once('ready', function() {
+                        loop(i+1);
+                      });
+            });
 }
 
 function average(l, field) {
-    var sum = 0;
-    l.forEach(function (e) {sum += e[field];});
-    return sum / l.length;
+  var sum = 0;
+  l.forEach(function (e) {sum += e[field];});
+  return sum / l.length;
 }
 
 function done() {
-    var results = [];
-    if (options.dump_stats)
-        fs.writeFileSync(options.dump_stats,
-                         JSON.stringify(times))
-    for (q in times) {
-        results.push([q, times[q], average(times[q], 'time')]);
+  var results = [];
+  if (options.dump_stats)
+    fs.writeFileSync(options.dump_stats,
+                     JSON.stringify(times))
+  for (q in times) {
+    results.push([q, times[q], average(times[q], 'time')]);
+  }
+  results.sort(function (a,b) {
+                 return b[2] - a[2]
+               });
+  console.log("*** RESULTS ***")
+  function pad(str, len) {
+    while (str.length < len)
+      str += ' ';
+    return str;
+  }
+  function fmt(re) {
+    var WIDTH = 20;
+    if (re.length < WIDTH) {
+      re = pad(re, WIDTH);
     }
-    results.sort(function (a,b) {
-                     return b[2] - a[2]
-                 });
-    console.log("*** RESULTS ***")
-    function pad(str, len) {
-        while (str.length < len)
-            str += ' ';
-        return str;
+    if (re.length > WIDTH) {
+      var start = re.substr(0, WIDTH / 2);
+      var end   = re.substring(re.length - (WIDTH - start.length - 3));
+      re = start + '...' + end;
     }
-    function fmt(re) {
-        var WIDTH = 20;
-        if (re.length < WIDTH) {
-            re = pad(re, WIDTH);
-        }
-        if (re.length > WIDTH) {
-            var start = re.substr(0, WIDTH / 2);
-            var end   = re.substring(re.length - (WIDTH - start.length - 3));
-            re = start + '...' + end;
-        }
-        return re;
-    }
-    results.forEach(function (r) {
-        var matches = r[1].map(function (f) { return f.nmatch });
-        var min_match = Math.min.apply(Math, matches);
-        var max_match = Math.min.apply(Math, matches);
-        console.log("[%s]: %ss (re2: %s, index: %s) [%d, %d]",
-                    fmt(r[0]),
-                    pad(''+Math.round(r[2])/1000, 6),
-                    Math.round(average(r[1], 're2_time'))/1000,
-                    Math.round(average(r[1], 'index_time'))/1000,
-                    min_match, max_match);
-    });
-    process.exit(0);
+    return re;
+  }
+  results.forEach(function (r) {
+                    var matches = r[1].map(function (f) { return f.nmatch });
+                    var min_match = Math.min.apply(Math, matches);
+                    var max_match = Math.min.apply(Math, matches);
+                    console.log("[%s]: %ss (re2: %s, index: %s) [%d, %d]",
+                                fmt(r[0]),
+                                pad(''+Math.round(r[2])/1000, 6),
+                                Math.round(average(r[1], 're2_time'))/1000,
+                                Math.round(average(r[1], 'index_time'))/1000,
+                                min_match, max_match);
+                  });
+  process.exit(0);
 }
 
 cs.once('ready', function() {
-            console.log("Begin searching...");
-            loop(0);
+          console.log("Begin searching...");
+          loop(0);
         });
