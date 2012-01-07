18f187cc0de58ef752fbe5938ff7e1f2ca5db61b
Nelson Elhage <nelhage@nelhage.com>
2012-01-07 15:36:47 -0500

    Service clients in random order for fairness.

--- web/appserver.js
+++ web/appserver.js
@@ -112,9 +112,19 @@ function SearchServer() {
   }
 }
 
+function shuffle(lst) {
+  for (var i = lst.length - 1; i >= 0; i--) {
+    var j = Math.floor(Math.random() * (i+1));
+    var tmp = lst[i];
+    lst[i] = lst[j];
+    lst[j] = tmp;
+  }
+  return lst;
+}
+
 SearchServer.prototype.dispatch = function () {
   var clients = this.clients;
-  Object.keys(this.clients).forEach(
+  shuffle(Object.keys(this.clients)).forEach(
     function (id) {
       clients[id].dispatch_search();
     })
