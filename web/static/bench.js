4e8b1f673d2e5881db6d0e369c914883f808a6e0
Nelson Elhage <nelhage@nelhage.com>
2012-01-20 09:11:29 -0500

    Web benchmark: Display the max query time in the last 5s.

--- web/static/bench.js
+++ web/static/bench.js
@@ -14,24 +14,36 @@ var Benchmark = function() {
     "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
   ];
 
+  function max_time() {
+    var max = 0;
+    Benchmark.responses.forEach(function (r) {
+                                  if (r.time > max)
+                                    max = r.time;
+                                });
+    return max;
+  }
+
   function render() {
     var now = new Date();
     var ms = +(now - Benchmark.start_time)
     $("#val_time").text(ms);
     $("#val_searches").text(Benchmark.searches);
     $("#val_errors").text(Benchmark.errors);
-    $("#val_qps").text(1000 * Benchmark.searches / ms);
     $("#val_window").text(1000 * Benchmark.responses.length /
-        (now - Benchmark.responses[0]));
+        (now - Benchmark.responses[0].end));
+    $("#val_max").text(max_time());
   }
 
   function done(error) {
+    var now = new Date();
     Benchmark.searches++;
     if (error)
       Benchmark.errors++;
-    Benchmark.responses.push(new Date());
-    var now = new Date();
-    while ((now - Benchmark.responses[0]) > WINDOW)
+    Benchmark.responses.push({
+                               end: now,
+                               time: now - Benchmark.search_start
+                             });
+    while ((now - Benchmark.responses[0].end) > WINDOW)
       Benchmark.responses.shift(1);
     render();
   }
@@ -39,12 +51,14 @@ var Benchmark = function() {
   function loop(i) {
     if (i === queries.length)
       i = 0;
+    Benchmark.search_start = new Date();
     Codesearch.new_search(queries[i]);
     Benchmark.timer = setTimeout(function() {loop(i+1)}, 10);
   }
 
   return {
     start_time: 0,
+    search_start: 0,
     searches: 0,
     errors: 0,
     responses: [],
