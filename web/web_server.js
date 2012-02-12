6d2e2eddb1e9eea061c099cb59b0044f4e1f91a9
Nelson Elhage <nelhage@nelhage.com>
2012-02-12 16:20:43 -0500

    Stop trying to proxy socket.io via nginx

--- web/web_server.js
+++ web/web_server.js
@@ -50,36 +50,6 @@ var io = require('socket.io').listen(app, {
 io.configure(
   function() {
     io.enable('browser client gzip');
-    io.set("transports", ["xhr-polling"]);
-    io.set("polling duration", 10);
-
-    var path = require('path');
-    var HTTPPolling = require(path.join(
-                                path.dirname(require.resolve('socket.io')),'lib', 'transports','http-polling')
-                             );
-    var XHRPolling = require(path.join(
-                               path.dirname(require.resolve('socket.io')),'lib','transports','xhr-polling')
-                            );
-
-    XHRPolling.prototype.doWrite = function(data) {
-      HTTPPolling.prototype.doWrite.call(this);
-
-      var headers = {
-        'Content-Type': 'text/plain; charset=UTF-8',
-        'Content-Length': (data && Buffer.byteLength(data)) || 0
-      };
-
-      if (this.req.headers.origin) {
-        headers['Access-Control-Allow-Origin'] = '*';
-        if (this.req.headers.cookie) {
-          headers['Access-Control-Allow-Credentials'] = 'true';
-        }
-      }
-
-      this.response.writeHead(200, headers);
-      this.response.write(data);
-      this.log.debug(this.name + ' writing', data);
-    };
   });
 
 var server = new Server(config, io);
