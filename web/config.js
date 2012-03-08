bf37fb0886485102fe832c67909aae07ae6daaf8
Nelson Elhage <nelhage@nelhage.com>
2012-03-07 22:44:54 -0500

    Drop the default number of backend connections to 4.

--- web/config.js
+++ web/config.js
@@ -7,7 +7,7 @@ var config = {
   SEARCH_REPO: path.join(__dirname, "../../linux"),
   SEARCH_REF:  "v3.0",
   SEARCH_ARGS: [],
-  BACKEND_CONNECTIONS: 8,
+  BACKEND_CONNECTIONS: 4,
   BACKENDS: [
     ["localhost", 0xC5EA]
   ],
