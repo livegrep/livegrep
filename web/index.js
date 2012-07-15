0fdff9ce4ee27ac40492603f45d892495df6dfd8
Nelson Elhage <nelhage@nelhage.com>
2012-07-15 15:20:38 -0400

    index.js: Write the index to a temporary file and then rename.

--- web/index.js
+++ web/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 var path    = require('path'),
+    fs      = require('fs'),
     config  = require('./config.js'),
     spawn   = require('child_process').spawn;
 
@@ -7,9 +8,11 @@ console.log("Generating index file:")
 console.log("  %s", config.SEARCH_INDEX),
 console.log("from %s, ref:%s...", config.SEARCH_REPO, config.SEARCH_REF);
 
+var tmp = config.SEARCH_INDEX + ".tmp";
+
 var cs = spawn(path.join(__dirname, '..', 'codesearch'),
                ['--git_dir', path.join(config.SEARCH_REPO, ".git"),
-                '--dump_index', config.SEARCH_INDEX,
+                '--dump_index', tmp,
                 '--order_root', config.ORDER_DIRS.join(' '),
                 config.SEARCH_REF],
                {
@@ -18,6 +21,7 @@ var cs = spawn(path.join(__dirname, '..', 'codesearch'),
 cs.on('exit', function(code) {
         if (code !== 0)
           console.error("Index process exited with error %d", code);
+        fs.renameSync(tmp, config.SEARCH_INDEX);
         process.exit(0);
       });
 cs.stdin.end();
