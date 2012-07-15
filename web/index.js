9e86edbe1efc12846b225c68bb5c923cdbfc5217
Nelson Elhage <nelhage@nelhage.com>
2012-07-15 15:06:00 -0400

    index.js: Print some info about what we're doing.

--- web/index.js
+++ web/index.js
@@ -3,6 +3,10 @@ var path    = require('path'),
     config  = require('./config.js'),
     spawn   = require('child_process').spawn;
 
+console.log("Generating index file:")
+console.log("  %s", config.SEARCH_INDEX),
+console.log("from %s, ref:%s...", config.SEARCH_REPO, config.SEARCH_REF);
+
 var cs = spawn(path.join(__dirname, '..', 'codesearch'),
                ['--git_dir', path.join(config.SEARCH_REPO, ".git"),
                 '--dump_index', config.SEARCH_INDEX,
