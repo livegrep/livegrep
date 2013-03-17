93b0f1963edeba5df5927f8090795a2ee0c2cae0
Nelson Elhage <nelhage@nelhage.com>
2013-03-16 23:13:36 -0700

    Optionally generate dot graphs of the index keys.

--- src/tools/analyze-re.cc
+++ src/tools/analyze-re.cc
@@ -8,6 +8,7 @@
 #include <assert.h>
 #include <string.h>
 #include <algorithm>
+#include <fstream>
 
 #include <string>
 
@@ -16,6 +17,81 @@
 #include "debug.h"
 #include "indexer.h"
 #include "re_width.h"
+
+using namespace std;
+
+DEFINE_string(dot_index, "", "Write a graph of the index key as a dot graph.");
+
+class IndexKeyDotOutputter {
+protected:
+    map<IndexKey*, string> names_;
+    set<IndexKey*> seen_;
+    ofstream out_;
+    int ct_;
+    intrusive_ptr<IndexKey> key_;
+
+    string escape(char c) {
+        if (c <= ' ' || c > '~' || c == '"' || c == '\\')
+            return strprintf("\\\\x%hhx", c);
+        return strprintf("%c", c);
+    }
+
+    void assign_names(intrusive_ptr<IndexKey> key) {
+        if (names_.find(key.get()) != names_.end())
+            return;
+        names_[key.get()] = strprintf("node%d", ct_++);
+        out_ << strprintf("%s [label=\"%s\"]\n",
+                          names_[key.get()].c_str(),
+                          (key->anchor & kAnchorLeft) ? "^" :
+                          (key->anchor & kAnchorRight) ? "$" : "").c_str();
+        for (auto it = key->begin(); it != key->end(); it++) {
+            if (!it->second)
+                continue;
+            assign_names(it->second);
+        }
+    }
+
+    void dump(intrusive_ptr<IndexKey> key) {
+        if (seen_.find(key.get()) != seen_.end())
+            return;
+        seen_.insert(key.get());
+        for (auto it = key->begin(); it != key->end(); it++) {
+            if (!it->second)
+                continue;
+            string label;
+            if (it->first.first == it->first.second)
+                label = escape(it->first.first);
+            else
+                label = strprintf("%s-%s",
+                                  escape(it->first.first).c_str(),
+                                  escape(it->first.second).c_str());
+            out_ << strprintf("%s -> %s [label=\"%s\"]\n",
+                              names_[key.get()].c_str(),
+                              names_[it->second.get()].c_str(),
+                              label.c_str());
+            dump(it->second);
+        }
+    }
+
+public:
+    IndexKeyDotOutputter(const string &path, intrusive_ptr<IndexKey> key)
+        : out_(path.c_str()), ct_(0), key_(key) {
+    }
+
+    void output() {
+        out_ << "digraph G {\n";
+        assign_names(key_);
+        dump(key_);
+        out_ << "}\n";
+        out_.close();
+    }
+};
+
+
+void write_dot_index(const string &path, intrusive_ptr<IndexKey> key) {
+    IndexKeyDotOutputter out(path, key);
+    out.output();
+}
 
 int main(int argc, char **argv) {
     google::SetUsageMessage("Usage: " + string(argv[0]) + " <options> REGEX");
@@ -47,6 +123,10 @@ int main(int argc, char **argv) {
         printf("  log10(selectivity): %f\n", log(stats.selectivity_)/log(10));
         printf("  depth: %d\n", stats.depth_);
         printf("  nodes: %ld\n", stats.nodes_);
+
+        if (FLAGS_dot_index.size()) {
+            write_dot_index(FLAGS_dot_index, key);
+        }
     } else {
         printf("(Unindexable)\n");
     }
