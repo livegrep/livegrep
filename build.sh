94cf245477460dee2c4471e8bd9a7eaa3fc6d7e5
Nelson Elhage <nelhage@nelhage.com>
2014-04-02 12:48:14 -0700

    some build.sh updates

--- build.sh
+++ build.sh
@@ -4,16 +4,17 @@ set -eux
 
 rev=$(git rev-parse HEAD | head -c10)
 builddir="livegrep-$rev"
+tgz="${1-livegrep-$rev.tgz}"
 
 rm -rf "$builddir"
 mkdir -p "$builddir"
 
-make -j4 LDFLAGS=-static
+make -j4 LDFLAGS=-static all
 
 cp -a codesearch analyze-re inspect-index "$builddir/"
 go build -o "$builddir/livegrep" "github.com/nelhage/livegrep/livegrep"
 
 cp -a web "$builddir/"
 
-tar cvzf "livegrep-$rev.tgz" "$builddir/"
+tar cvzf "$tgz" "$builddir/"
 rm -rf "$builddir"
