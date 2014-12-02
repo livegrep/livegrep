0dd24032f144c0665fa65240cbc2645cea828750
Nelson Elhage <nelhage@nelhage.com>
2014-12-01 22:56:29 -0500

    basic crosscheck against `git grep`

--- client/test/integration_test.go
+++ client/test/integration_test.go
@@ -1,10 +1,16 @@
 package test
 
 import (
+	"bufio"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"regexp"
+	"sort"
+	"strconv"
 	"testing"
 
 	"github.com/livegrep/livegrep/client"
@@ -57,7 +63,11 @@ func (i *IntegrationSuite) SetUpSuite(c *check.C) {
 		c.Fatalf("Encode: %s", err)
 	}
 
-	i.client, err = NewClient("--dump_index", i.index.Name(), i.config.Name())
+	i.client, err = NewClient(
+		"--dump_index", i.index.Name(),
+		"--max_matches", "100000",
+		"--timeout", "100000",
+		i.config.Name())
 	if err != nil {
 		c.Fatalf("NewClient: %s", err)
 	}
@@ -87,4 +97,80 @@ func (i *IntegrationSuite) TestBasic(c *check.C) {
 	stats, err := search.Close()
 	c.Check(stats, check.Not(check.IsNil))
 	c.Check(err, check.IsNil)
+}
+
+type Match struct {
+	Path string
+	Line int
+}
+
+type SortMatches []Match
+
+func (s SortMatches) Less(i, j int) bool {
+	l, r := s[i], s[j]
+	if l.Path != r.Path {
+		return l.Path < r.Path
+	}
+	return l.Line < r.Line
+}
+func (s SortMatches) Len() int {
+	return len(s)
+}
+
+func (s SortMatches) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+var grepRE = regexp.MustCompile(`^HEAD:([^:]+):(\d+):`)
+
+func gitGrep(path, regex string) ([]Match, error) {
+	cmd := exec.Command("git", "grep", "-n", "-E", "-e", regex, "HEAD")
+	cmd.Dir = path
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	var matches []Match
+
+	cmd.Start()
+	scan := bufio.NewScanner(out)
+	for scan.Scan() {
+		line := scan.Bytes()
+		m := grepRE.FindSubmatch(line)
+		if m == nil {
+			return nil, fmt.Errorf("unparsable: `%s'", line)
+		}
+		lno, _ := strconv.Atoi(string(m[2]))
+		matches = append(matches, Match{string(m[1]), lno})
+	}
+	cmd.Wait()
+
+	return matches, nil
+}
+
+func (i *IntegrationSuite) crosscheck(c *check.C, regex string) {
+	gitMatches, err := gitGrep(*repo, regex)
+	if err != nil {
+		c.Fatalf("git grep: %s", err)
+	}
+
+	var livegrepMatches []Match
+	search, err := i.client.Query(&client.Query{Line: regex})
+	if err != nil {
+		c.Fatalf("Query: %s", err)
+	}
+	for m := range search.Results() {
+		livegrepMatches = append(livegrepMatches, Match{m.Path, m.LineNumber})
+	}
+	search.Close()
+
+	sort.Sort(SortMatches(gitMatches))
+	sort.Sort(SortMatches(livegrepMatches))
+
+	c.Check(livegrepMatches, check.DeepEquals, gitMatches)
+}
+
+func (i *IntegrationSuite) TestCrosscheck(c *check.C) {
+	i.crosscheck(c, `hello`)
 }
