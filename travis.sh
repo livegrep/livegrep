23f562698031f74a658553050c20efcc4870fe99
Nelson Elhage <nelhage@nelhage.com>
2016-08-14 19:25:38 -0700

    Add bazel build in Travis

--- travis.sh
+++ travis.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
-set -ex
+set -e
 
-mkdir -p deps
+bazel build //...
+bazel test //...
 
-if ! test -d deps/linux; then
-    git clone --depth=1 --branch v3.17 https://github.com/torvalds/linux deps/linux
-fi
-
-if ! test -d deps/gflags; then
-    git clone -b v2.1.2 --depth=1 https://github.com/gflags/gflags.git deps/gflags
-    cd deps/gflags
-    mkdir build
-    cd build
-    cmake ..
-    make -j2
-    cd ../../..
-fi
-
-if ! test -d deps/libgit2; then
-    cd deps
-    wget https://github.com/libgit2/libgit2/archive/v0.21.0.tar.gz
-    tar xzf v0.21.0.tar.gz
-    mv libgit2-0.21.0 libgit2
-    cd libgit2
-    mkdir build
-    cd build
-    cmake -DBUILD_SHARED_LIBS=false -DCMAKE_INSTALL_PREFIX=$(pwd) ..
-    cmake --build . --target install
-    cd ../../..
-fi
+bazel-bin/client/test/go_default_test -test.repo "$(pwd)/deps/linux"
