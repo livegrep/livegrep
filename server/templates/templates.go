e8001859094b56b841076532f673148ddf867380
Josiah Boning <josiah@dropbox.com>
2017-09-22 23:05:03 +0000

    Fetch static resources with content hashes
    This ensures that users don't see old cached versions of the javascript.
    
    Enforcing subresource integrity here is probably unneeded, but we have
    the information, so why not?

--- server/templates/templates.go
+++ server/templates/templates.go
@@ -1,9 +1,13 @@
 package templates
 
 import (
+	"bufio"
+	"encoding/base64"
+	"encoding/hex"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path"
 	"reflect"
 	"strings"
@@ -17,14 +21,33 @@ func templatePath(f reflect.StructField) string {
 	return strings.ToLower(f.Name) + ".html"
 }
 
+func linkTag(rel string, s string, m map[string]string) template.HTML {
+	hash := m[strings.TrimPrefix(s, "/")]
+	href := s + "?v=" + hash
+	hashBytes, _ := hex.DecodeString(hash)
+	integrity := "sha256-" + base64.StdEncoding.EncodeToString(hashBytes)
+	return template.HTML(`<link rel="` + rel + `" href="` + href + `" integrity="` + integrity + `" />`)
+}
+
+func scriptTag(s string, m map[string]string) template.HTML {
+	hash := m[strings.TrimPrefix(s, "/")]
+	href := s + "?v=" + hash
+	hashBytes, _ := hex.DecodeString(hash)
+	integrity := "sha256-" + base64.StdEncoding.EncodeToString(hashBytes)
+	return template.HTML(`<script src="` + href + `" integrity="` + integrity + `"></script>`)
+
+}
+
 func getFuncs() map[string]interface{} {
 	return map[string]interface{}{
 		"loop":      func(n int) []struct{} { return make([]struct{}, n) },
 		"toLineNum": func(n int) int { return n + 1 },
+		"linkTag":   linkTag,
+		"scriptTag": scriptTag,
 	}
 }
 
-func Load(base string, templates interface{}) error {
+func LoadTemplates(base string, templates interface{}) error {
 	v := reflect.ValueOf(templates)
 	if v.Kind() != reflect.Ptr {
 		panic("Load: Must provide pointer-to-struct")
@@ -61,18 +84,53 @@ func Load(base string, templates interface{}) error {
 	return nil
 }
 
-type reloadHandler struct {
-	baseDir string
-	t       interface{}
-	in      http.Handler
+func LoadAssetHashes(assetHashFile string, assetHashMap map[string]string) error {
+	file, err := os.Open(assetHashFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for k := range assetHashMap {
+		delete(assetHashMap, k)
+	}
+
+	for scanner.Scan() {
+		pieces := strings.SplitN(scanner.Text(), "  ", 2)
+		hash := pieces[0]
+		asset := pieces[1]
+		(assetHashMap)[asset] = hash
+	}
+
+	return nil
 }
 
-func ReloadHandler(base string, templates interface{}, h http.Handler) http.Handler {
-	return &reloadHandler{base, templates, h}
+func Load(base string, templates interface{}, assetHashFile string, assetHashMap map[string]string) error {
+	if err := LoadTemplates(base, templates); err != nil {
+		return err
+	}
+	if err := LoadAssetHashes(assetHashFile, assetHashMap); err != nil {
+		return err
+	}
+	return nil
+}
+
+type reloadHandler struct {
+	baseDir       string
+	t             interface{}
+	assetHashFile string
+	assetHashMap  map[string]string
+	in            http.Handler
+}
+
+func ReloadHandler(base string, templates interface{}, assetHashFile string, assetHashMap map[string]string, h http.Handler) http.Handler {
+	return &reloadHandler{base, templates, assetHashFile, assetHashMap, h}
 }
 
 func (h *reloadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	e := Load(h.baseDir, h.t)
+	e := Load(h.baseDir, h.t, h.assetHashFile, h.assetHashMap)
 	if e != nil {
 		log.Printf("loading templates: err=%v", e)
 	}
