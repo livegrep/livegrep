c768b7649c8ab7ae196e1b2e9d9739a051dd936d
Nelson Elhage <nelhage@nelhage.com>
2017-10-02 20:50:47 -0700

    Copy the built assets into the package

--- package.sh
+++ package.sh
@@ -4,7 +4,8 @@ mkdir -p builds
 rev=$(git rev-parse HEAD | head -c10)
 builddir="livegrep-$rev"
 mkdir -p "$builddir"
-cp -a web doc README.md COPYING "$builddir"
+cp -a doc README.md COPYING "$builddir"
 mkdir -p "$builddir/bin"
 cp -a $(bazel query 'kind("cc_binary|genrule", //src/tools/...) union kind("go_binary", //cmd/...)' | sed -e s,^/,bazel-bin, -e s,:,/, -e s,/tool-,/,) "$builddir/bin"
+cp -aL bazel-bin/cmd/livegrep/livegrep.runfiles/com_github_livegrep_livegrep/web "$builddir"
 tar -czf "builds/$builddir.tgz" "$builddir"
