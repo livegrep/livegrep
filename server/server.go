2d192fd3e29e375451727b10d5be2dc82ca79c7b
Utsav Shah <utsav@dropbox.com>
2018-07-19 15:26:10 -0700

    Continue after errors

--- server/server.go
+++ server/server.go
@@ -498,6 +498,7 @@ func New(cfg *config.Config) (http.Handler, error) {
 			// a failing language server isn't fatal. We'd prefer to log these metrics.
 			if err != nil {
 				log.Printf(ctx, "%s", err.Error())
+				continue
 			}
 
 			initParams := &langserver.InitializeParams{
@@ -512,6 +513,7 @@ func New(cfg *config.Config) (http.Handler, error) {
 			// a failing language server isn't fatal. We'd prefer to log these metrics.
 			if err != nil {
 				log.Printf(ctx, "%s", err.Error())
+				continue
 			}
 
 			srv.langsrv[langServer.Address] = client
