e7c0a5d5738a2a9de0f94740e0d8d83e16f9ae1f
Ainsley Escorce-Jones <ains@stripe.com>
2018-07-31 11:32:24 -0700

    Fix link clicking bevhaiour

--- web/src/fileview/fileview.js
+++ web/src/fileview/fileview.js
@@ -217,15 +217,6 @@ function init(initData) {
     return range;
   }
 
-  function triggerJumpToDef(event) {
-      const nodeClicked = event.target;
-      const cachedUrl = nodeClicked.getAttribute('definition-url');
-      if (cachedUrl) {
-        window.location.href = cachedUrl;
-        handleHashChange();
-      }
-  }
-
   var hoveringNode = null;
   var mousePositionX = 0;
   var mousePositionY = 0;
@@ -243,8 +234,6 @@ function init(initData) {
   }
 
   function checkIfHoverable(node) {
-    var info = getFileInfo();
-
     const code = document.getElementById('source-code');
     const stringBefore = textBeforeOffset(node, 0, code);
 
@@ -267,7 +256,12 @@ function init(initData) {
         .then(function(resp) {
             if (resp) {
                 node.classList.add('hoverable');
-                node.setAttribute('definition-url', resp.url);
+                var asLink = document.createElement('a');
+                asLink.setAttribute('href', resp.url);
+
+                node.parentNode.insertBefore(asLink, node);
+                asLink.appendChild(node);
+
                 if (isInBox(mousePositionX, mousePositionY, node.getBoundingClientRect())) {
                     hoverOverNode(node);
                 }
@@ -435,9 +429,13 @@ function init(initData) {
       processKeyEvent(event);
     });
 
-    $(document).on('click', '.hoverable', function (event) {
+    $(document).on('click', '.token a', function (event) {
       if (initData.has_lang_server) {
-        triggerJumpToDef(event);
+          // hashChange event wont fire here so we need to handle scrolling to the correct location ourselves.
+          if(this.hash === window.location.hash) {
+              handleHashChange();
+              event.preventDefault();
+          }
       }
     });
 
