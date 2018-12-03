d8f4b799dba5eb67d282bb13d6ae589a7f146563
Nelson Elhage <nelhage@nelhage.com>
2018-12-03 14:12:40 -0800

    Use `bazel info bazel-bin` in package.sh

--- package.sh
+++ package.sh
@@ -5,6 +5,6 @@ rev=$(git rev-parse HEAD | head -c10)
 builddir="livegrep-$rev"
 rm -rf "$builddir" && mkdir "$builddir"
 bazel build :livegrep
-tar -C "$builddir" -xf bazel-bin/livegrep.tar
+tar -C "$builddir" -xf "$(bazel info bazel-bin)"/livegrep.tar
 tar -czf "builds/$builddir.tgz" "$builddir"
 rm -rf "$builddir"
