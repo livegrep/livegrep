f4a1f445ce5a6183417be952ccd2eb21b3b73b18
Rodrigo Silva Mendoza <rodrigo.silvamendoza@nytimes.com>
2022-07-03 13:12:06 -0700

    Clean up comments and unused code

--- src/git_indexer.cc
+++ src/git_indexer.cc
@@ -39,13 +39,6 @@ git_indexer::~git_indexer() {
 }
 
 
-// Used to get the next index of a repo a thread should focus on
-int git_indexer::get_next_repo_idx() {
-    if (next_repo_to_process_idx_.load() == repositories_to_index_length_) {
-        return -1;
-    }
-    return next_repo_to_process_idx_.fetch_add(1, std::memory_order_relaxed);
-}
 
 void git_indexer::print_last_git_err_and_exit(int err) {
     const git_error *e = giterr_last();
@@ -53,94 +46,14 @@ void git_indexer::print_last_git_err_and_exit(int err) {
     exit(1);
 }
 
-/* void git_indexer::process_repos(int estimatedReposToProcess, threadsafe_progress_indicator *tpi) { */
-/*     int idx_to_process = get_next_repo_idx(); */
-/*     std::vector<git_repository *> open_git_repos_local; */
-/*     std::vector<std::unique_ptr<pre_indexed_file>> files_to_index_local; */
-
-/*     open_git_repos_local.reserve(estimatedReposToProcess); */
-/*     files_to_index_local.reserve(estimatedReposToProcess * 100); */
-
-/*     while (idx_to_process >= 0) { */
-/*         /1* fprintf(stderr, "going to process: %d\n", idx_to_process); *1/ */
-/*         /1* auto start = high_resolution_clock::now(); *1/ */
-/*         const auto &repo = repositories_to_index_[idx_to_process]; */
-/*         const char *repopath = repo.path().c_str(); */
-
-/*         /1* fprintf(stderr, "walking repo: %s\n", repopath); *1/ */
-/*         git_repository *curr_repo = NULL; */
-
-/*         // Is it safe to assume these are bare repos (or the mirror clones) */
-/*         // that we create?. If so, we can use git_repository_open_bare */
-/*         int err = git_repository_open_bare(&curr_repo, repopath); */
-/*         if (err < 0) { */
-/*             print_last_git_err_and_exit(err); */
-/*         } */
-
-/*         open_git_repos_local.push_back(curr_repo); */
-
-/*         /1* for (auto &rev : repo.revisions()) { *1/ */
-/*             /1* fprintf(stderr, "walking %s at %s \n", repopath, rev.c_str()); *1/ */
-/*             /1* walk(curr_repo, rev, repo.path(), repo.name(), repo.metadata(), repo.walk_submodules(), "", files_to_index_local); *1/ */
-/*             /1* fprintf(stderr, "done walking %s at %s\n", repopath, rev.c_str()); *1/ */
-/*             /1* tpi->tick(); *1/ */
-/*         /1* } *1/ */
-/*         /1* auto stop = high_resolution_clock::now(); *1/ */
-/*         /1* auto duration = duration_cast<seconds>(stop - start); *1/ */
-/*         /1* cout << "took: " << duration.count() << " seconds to process_repos" << endl; *1/ */
-
-/*         idx_to_process = get_next_repo_idx(); */
-/*     } */
-
-/*     std::lock_guard<std::mutex> guard(files_mutex_); */
-/*     files_to_index_.insert(files_to_index_.end(), std::make_move_iterator(files_to_index_local.begin()), std::make_move_iterator(files_to_index_local.end())); */
-/*     open_git_repos_.insert(open_git_repos_.end(), open_git_repos_local.begin(), open_git_repos_local.end()); */
-/* } */
 void git_indexer::process_trees(int thread_id) {
-    fprintf(stderr, "thread %d listening\n", thread_id);
     tree_to_walk *d;
 
     while (trees_to_walk_.pop(&d)) {
-        // got a t with
-        /* fprintf(stderr, "thread[%d] popped an elem off the stack\n", thread_id); */
-        /* std::cout << t; */
-
-        /* std::cout << "Name: " << d->name << " repopath: " << d->repopath << std::endl; */
-
-        /* std::cout << "repopath: " << d->repopath << std::endl; */
-        /* fprintf(stderr, "thread[%d]: %s/%s\n", thread_id, d->repopath.c_str(), d->prefix.c_str()); */
-
-        /* std::cout << "Id addr: " << tree << std::endl; */
-              /* << "walk_submodules: " << tree->walk_submodules << std::endl */
-              /* << "submodule_prefix: " << tree->submodule_prefix << std::endl */
-              /* << "idx_tree.name " << tree->idx_tree->name << std::endl; */
-        /* fprintf(stderr, "[%d]: walk_tree('%s','%s','%s')\n", thread_id, tree->prefix.c_str(), tree->order.c_str(), tree->repopath.c_str()); */
         walk_tree(d->prefix, d->order, d->repopath, d->walk_submodules, 
                 d->submodule_prefix, d->idx_tree, d->tree, d->repo, 1);
     }
 }
-
-/* void git_indexer::process_trees(int thread_id) { */
-/*     fprintf(stderr, "thread %d listening\n", thread_id); */
-/*     tree_to_walk *tree; */
-
-/*     while (trees_to_walk_.pop(&tree)) { */
-/*         // got a t with */
-/*         fprintf(stderr, "thread[%d] popped an elem off the stack\n", thread_id); */
-/*         /1* std::cout << t; *1/ */
-/*         std::cout << "Id: " << tree->id << std::endl */ 
-/*               << "Prefix: " << tree->prefix << std::endl */
-/*               << "Order: " << tree->order << std::endl */
-/*               << "repopath: " << tree->repopath << std::endl;; */
-/*         fprintf(stderr, "\n"); */
-/*               /1* << "walk_submodules: " << tree->walk_submodules << std::endl *1/ */
-/*               /1* << "submodule_prefix: " << tree->submodule_prefix << std::endl *1/ */
-/*               /1* << "idx_tree.name " << tree->idx_tree->name << std::endl; *1/ */
-/*         /1* fprintf(stderr, "[%d]: walk_tree('%s','%s','%s')\n", thread_id, tree->prefix.c_str(), tree->order.c_str(), tree->repopath.c_str()); *1/ */
-/*         walk_tree(tree->prefix, tree->order, tree->repopath, tree->walk_submodules, */ 
-/*                 tree->submodule_prefix, tree->idx_tree, tree->tree, tree->repo, 1); */
-/*     } */
-/* } */
 
 void git_indexer::begin_indexing() {
 
@@ -192,56 +105,12 @@ void git_indexer::begin_indexing() {
         open_git_repos_.push_back(curr_repo);
 
         for (auto &rev : repo.revisions()) {
-            /* fprintf(stderr, "walking %s at %s \n", repopath, rev.c_str()); */
             walk(curr_repo, rev, repo.path(), repo.name(), repo.metadata(), repo.walk_submodules(), "");
-            /* tree_to_walk t{ */
-            /*     std::rand(), */
-            /*     "/", */
-            /*     "", */
-            /*     repo.path(), */
-            /*     false, */
-            /*     "", */
-            /* }; */
-            /* tree_to_walk t{}; */
-            /* t.repopath = repopath; */
-            /* t.prefix = path + "/"; */
-            
-        /* fprintf(stderr, "t is initially ----\n"); */
-        /* fprintf(stderr, "repopath.size(): %lu\n", repopath.size()); */
-        /* std::cout << "Prefix: " << t.prefix << std::endl */
-        /*       << "Order: " << t.order << std::endl */
-        /*       << "repopath: " << t.repopath << std::endl */
-        /*       << "walk_submodules: " << t.walk_submodules << std::endl */
-        /*       << "submodule_prefix: " << t.submodule_prefix << std::endl */
-        /*       << "idx_tree.name " << t.idx_tree->name << std::endl; */
-            /* fprintf(stderr, "about to push to trees_to_walk_ id: %d\n", t.id); */
-            /* trees_to_walk_.push(&t); */
-            /* fprintf(stderr, "done walking %s at %s\n", repopath, rev.c_str()); */
         }
-        /* tpi.tick(); */
+        tpi.tick();
      }
 
-     // ahhh, this may have to be rethought. When we're done walking repos
-     // it doesn't mean that all of the subdirectories have been traversed
-     // So if we close this, we may run into problems.
-
-
-
-    /* if (length < 2 * min_per_thread) { */
-    /*     fprintf(stderr, "Not going to create any new threads.\n"); */
-    /*     process_repos(length, &tpi); */
-    /*     index_files(); */
-    /*     return; */
-    /* } */
-
-    /* int estimatedReposPerThread = length / num_threads; */
-    /* threads_.reserve(num_threads - 1); */
-    /* auto start = high_resolution_clock::now(); */
-    /* for (long i = 0; i < num_threads; ++i) { */
-    /*     threads_.emplace_back(&git_indexer::process_repos, this, estimatedReposPerThread, &tpi); */
-    /* } */
-
-    fprintf(stderr, "waiting for threads\n");
+    fprintf(stderr, "walking inner trees trees...\n");
 
     // we can close the trees, since we only add to trees_to_walk_ at the
     // root/unordered level
@@ -249,6 +118,7 @@ void git_indexer::begin_indexing() {
     for (long i = 0; i < num_threads; ++i) {
         threads_[i].join();
     }
+    fprintf(stderr, "    done.\n");
 
     // but we can't close the fq_ until all trees have been processed
     fq_.close();
@@ -256,11 +126,10 @@ void git_indexer::begin_indexing() {
     auto duration = duration_cast<milliseconds>(stop - start);
     cout << "took: " << duration.count() << " milliseconds to process_repos" << endl;
     /* exit(0); */
+
     pre_indexed_file *p;
 
-
     while (fq_.pop(&p)) {
-        /* fprintf(stderr, "pushing back some stuff\n"); */
         files_to_index_.push_back(p);
     }
     fprintf(stderr, "done waiting\n");
@@ -270,11 +139,11 @@ void git_indexer::begin_indexing() {
     index_files();
 }
 
-bool compareFilesByScore(const std::unique_ptr<pre_indexed_file>& a, const std::unique_ptr<pre_indexed_file>& b) {
+bool compareFilesByScore(pre_indexed_file *a, pre_indexed_file *b) {
     return a->score > b->score;
 }
 
-bool compareFilesByTree(const std::unique_ptr<pre_indexed_file>& a, const std::unique_ptr<pre_indexed_file>& b) {
+bool compareFilesByTree(pre_indexed_file *a, pre_indexed_file *b) {
     return a->tree->name < b->tree->name;
 }
 
@@ -288,13 +157,18 @@ bool compareFilesByTree(const std::unique_ptr<pre_indexed_file>& a, const std::u
 // walks `files_to_index_`, looks up the repo & blob combination for each file
 // and then calls `cs->index_file` to actually index the file.
 void git_indexer::index_files() {
-    /* fprintf(stderr, "sorting files_to_index_ by tree... [%lu]\n", files_to_index_.size()); */
-    /* std::stable_sort(files_to_index_.begin(), files_to_index_.end(), compareFilesByTree); */
-    /* fprintf(stderr, "  done\n"); */
+    auto start = high_resolution_clock::now();
+    fprintf(stderr, "sorting files_to_index_ by tree... [%lu]\n", files_to_index_.size());
+    std::stable_sort(files_to_index_.begin(), files_to_index_.end(), compareFilesByTree);
+    fprintf(stderr, "  done\n");
 
-    /* fprintf(stderr, "sorting files_to_index_ by score... [%lu]\n", files_to_index_.size()); */
-    /* std::stable_sort(files_to_index_.begin(), files_to_index_.end(), compareFilesByScore); */
-    /* fprintf(stderr, "  done\n"); */
+    fprintf(stderr, "sorting files_to_index_ by score... [%lu]\n", files_to_index_.size());
+    std::stable_sort(files_to_index_.begin(), files_to_index_.end(), compareFilesByScore);
+    fprintf(stderr, "  done\n");
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "took: " << duration.count() << " milliseconds to sort repos twice" << endl;
+
 
     /* fprintf(stderr, "walking files_to_index_ ...\n"); */
     threadsafe_progress_indicator tpi(files_to_index_.size(), "Indexing files_to_index_...", "Done");
@@ -353,20 +227,9 @@ void git_indexer::walk_tree(std::string pfx,
                             git_repository *curr_repo,
                             int depth) {
     /* fprintf(stderr, "[%d] preparing to walk_tree for %s with prefix: %s \n", depth, repopath.c_str(), pfx.c_str()); */
-    /* if (depth == 1) { */
-    /*     return; */
-    /* } */
     map<string, const git_tree_entry *> root;
     int entries = git_tree_entrycount(tree);
 
-    /* fprintf(stderr, "repo: %s has %d entries\n", repopath.c_str(), entries); */
-    /* fprintf(stderr, "%s/%s filled git_tree_entry map ok\n", repopath.c_str(), pfx.c_str()); */
-
-    /* for (map<string, const git_tree_entry *>::iterator it = root.begin(); */
-    /*      it != root.end(); ++it) */
-    /*     ordered.push_back(it->second); */
-
-    /* for (vector<const git_tree_entry *>::iterator it = ordered.begin(); it != ordered.end(); ++it) { */
     for (int i = 0; i < entries; ++i) {
         const git_tree_entry *ent = git_tree_entry_byindex(tree, i);
         
@@ -377,14 +240,8 @@ void git_indexer::walk_tree(std::string pfx,
         /* fprintf(stderr, "walking obj with path: %s/%s\n", repopath.c_str(), path.c_str()); */
 
         if (git_tree_entry_type(ent) == GIT_OBJ_TREE) {
-            /* fprintf(stderr, "entry is git_tree\n"); */
-            // But more likely, when depth == 1, we could add these to a thread
-            // pool. In that way, a repo could "potentially" be walked by 10
-            // threads at a time
-            /* fprintf(stderr, "going to add repopath with %s\n", repopath.c_str()); */
             if (depth == 1) { // don't add to the thread workload
                 walk_tree(path + "/", "", repopath, walk_submodules, submodule_prefix, idx_tree, obj, curr_repo, 1);
-                /* return; */
             } else {
                 tree_to_walk *t = new tree_to_walk;
                 t->prefix = path + "/";
@@ -399,48 +256,9 @@ void git_indexer::walk_tree(std::string pfx,
                 t->tree = (git_tree *)(obj1);
                 t->repo = curr_repo;
 
-
                 trees_to_walk_.push(t);
-
             }
-
-
-            /* auto tree = std::make_unique<tree_to_walk>(); */
-            /* tree_to_walk t{ */
-            /*     std::rand(), */
-            /*     path + "/", */
-            /*     "", */
-            /*     repopath + "/", */
-            /*     walk_submodules, */
-            /*     submodule_prefix, */
-            /*     idx_tree, */
-            /*     obj, */
-            /*     curr_repo, */
-            /* }; */
-            /* tree_to_walk t{}; */
-            /* t.repopath = repopath; */
-            /* t.prefix = path + "/"; */
-            
-        /* fprintf(stderr, "t is initially ----\n"); */
-        /* fprintf(stderr, "repopath.size(): %lu\n", repopath.size()); */
-        /* std::cout << "Prefix: " << t.prefix << std::endl */
-        /*       << "Order: " << t.order << std::endl */
-        /*       << "repopath: " << t.repopath << std::endl */
-        /*       << "walk_submodules: " << t.walk_submodules << std::endl */
-        /*       << "submodule_prefix: " << t.submodule_prefix << std::endl */
-        /*       << "idx_tree.name " << t.idx_tree->name << std::endl; */
-            /* fprintf(stderr, "about to push to trees_to_walk_ id: %d\n", t.id); */
-            /* trees_to_walk_.push(&t); */
-
-            /* dummy *d = new dummy; */
-            /* d->name = "hello = " + repopath; */
-            /* d->repopath = repopath; */
-            /* trees_to_walk_.push(d); */
-
-            /* walk_tree(path + "/", "", repopath, walk_submodules, submodule_prefix, idx_tree, obj, curr_repo, results); */
         } else if (git_tree_entry_type(ent) == GIT_OBJ_BLOB) {
-            /* fprintf(stderr, "entry is blob\n"); */
-
             const string full_path = submodule_prefix + path;
 
             pre_indexed_file *file = new pre_indexed_file;
@@ -453,8 +271,6 @@ void git_indexer::walk_tree(std::string pfx,
             file->oid = (git_oid *)malloc(sizeof(git_oid));
             git_oid_cpy(file->oid, git_blob_id(obj));
 
-            /* results.push_back(std::move(file)); */
-            /* fprintf(stderr, "about to push to global fq_\n"); */
             fq_.push(file);
         } else if (git_tree_entry_type(ent) == GIT_OBJ_COMMIT) {
             // Submodule
