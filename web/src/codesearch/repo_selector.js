be81e6055c0154386882f888469879677c44ed7a
ivanchgeek <70972568+ivanchgeek@users.noreply.github.com>
2023-06-27 17:45:45 +0300

    Add repos grouping by their path. (#370)
    * Add repos grouping by their path, add ability to select/unselect whole groups.
    
    * Repo names sorted in lexicographic order.
    
    * avoid excess jQuery search
    
    ---------
    
    Co-authored-by: root <root@dev-ivanchgeek.dev-ivanchgeek>

--- web/src/codesearch/repo_selector.js
+++ web/src/codesearch/repo_selector.js
@@ -10,6 +10,17 @@ function init() {
         liveSearch: true,
         width: '20em'
     });
+    $('#repos').on('refreshed.bs.select', function () {
+        var headers = $(this).parent().find('.dropdown-header');
+        headers.css('cursor', 'pointer');
+        headers.on('click', function (event) {
+            event.stopPropagation();
+            var optgroup = $('#repos optgroup[label="' + $(this).text() + '"]')
+            var allSelected = !optgroup.children('option:not(:selected)').length;
+            optgroup.children().prop('selected', !allSelected);
+            $("#repos").selectpicker('refresh');
+        });
+    });
     $(window).on('keyup', '.bootstrap-select .bs-searchbox input', function(event) {
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if(keycode == '13'){
@@ -37,10 +48,25 @@ function updateOptions(newOptions) {
     }
 
     $('#repos').empty();
+
+    newOptions.sort()
+    var groups = new Map()
+    groups.set('/', $('#repos'))
+
     for (var i = 0; i < newOptions.length; i++) {
-        var option = newOptions[i];
-        $('#repos').append($('<option>').attr('value', option).text(option));
+        var path = newOptions[i].split('/');
+        var group = path.slice(0, path.length - 1).join('/') + '/';
+        var option = path[path.length - 1];
+
+        if (!groups.has(group)) {
+            var groupDOM = $('<optgroup>').attr('label', group)
+            $('#repos').append(groupDOM);
+            groups.set(group, groupDOM)
+        }
+        groups.get(group).append($('<option>').attr('value', group + option).text(option));
     }
+
+    groups.clear()
     $('#repos').selectpicker('refresh');
 }
 
