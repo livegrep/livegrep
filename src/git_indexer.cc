livegrep/livegrep

commit 1e47ee8a11e3dad8554faa70785dedc15b80b195

Gracefully handle return status of git_tree_entry_to_object in git indexer (#366)

src/git_indexer.cc
@@ -83,7 +83,16 @@ void git_indexer::walk_tree(const string& pfx,
     for (vector<const git_tree_entry *>::iterator it = ordered.begin();
          it != ordered.end(); ++it) {
         smart_object<git_object> obj;
-        git_tree_entry_to_object(obj, repo_, *it);
+        char oid[GIT_OID_HEXSZ + 1];
+
+        const git_oid* entry_oid = git_tree_entry_id(*it);
+        git_oid_tostr(oid, GIT_OID_HEXSZ + 1, entry_oid);
+
+        if (git_tree_entry_to_object(obj, repo_, *it) != 0) {
+            fprintf(stderr, "Unable to convert git tree entry %s to object, skipping\n", oid);
+            continue;
+        }
+
         string path = pfx + git_tree_entry_name(*it);
 
         if (git_tree_entry_type(*it) == GIT_OBJ_TREE) {
@@ -108,11 +117,7 @@ void git_indexer::walk_tree(const string& pfx,
             git_indexer sub_indexer(cs_, sub_repopath, string(sub_name), meta, walk_submodules_);
             sub_indexer.submodule_prefix_ = submodule_prefix_ + path + "/";
 
-            const git_oid* rev = git_tree_entry_id(*it);
-            char revstr[GIT_OID_HEXSZ + 1];
-            git_oid_tostr(revstr, GIT_OID_HEXSZ + 1, rev);
-
-            sub_indexer.walk(string(revstr));
+            sub_indexer.walk(string(oid));
         }
     }
 }

commit 617368c7c5f05e8b3750e527c445519476821069

Fix indexing of git repositories with submodules
This commit fixes a regression caused by commit
1e47ee8a11e3dad8554faa70785dedc15b80b195.

In case of a git repository with submodules, the git_tree returns
git_tree_entry objects of type GIT_OBJECT_COMMIT that represent
the submodules, but cannot be converted to a git_object by
git_tree_entry_to_object(), because the commit is part of the submodule
and not of the repo itself.

This commit just moves the check, if the git_tree_entry_to_object()
call was successful, into the branches where the returned git_object is
actually used. For the GIT_OBJECT_COMMIT entries only the path is
used, not the git_object, therefor no check is needed there.

src/git_indexer.cc
@@ -88,18 +88,25 @@ void git_indexer::walk_tree(const string& pfx,
         const git_oid* entry_oid = git_tree_entry_id(*it);
         git_oid_tostr(oid, GIT_OID_HEXSZ + 1, entry_oid);
 
-        if (git_tree_entry_to_object(obj, repo_, *it) != 0) {
-            fprintf(stderr, "Unable to convert git tree entry %s to object, skipping\n", oid);
-            continue;
-        }
+        const bool is_object_from_repo = (git_tree_entry_to_object(obj, repo_, *it) == 0);
 
         string path = pfx + git_tree_entry_name(*it);
 
         if (git_tree_entry_type(*it) == GIT_OBJ_TREE) {
-            walk_tree(path + "/", "", obj);
+            if (is_object_from_repo) {
+                walk_tree(path + "/", "", obj);
+            } else {
+                fprintf(stderr, "Unable to convert git tree entry %s to object, skipping\n", oid);
+                continue;
+            }
         } else if (git_tree_entry_type(*it) == GIT_OBJ_BLOB) {
-            const char *data = static_cast<const char*>(git_blob_rawcontent(obj));
-            cs_->index_file(idx_tree_, submodule_prefix_ + path, StringPiece(data, git_blob_rawsize(obj)));
+            if (is_object_from_repo) {
+                const char *data = static_cast<const char*>(git_blob_rawcontent(obj));
+                cs_->index_file(idx_tree_, submodule_prefix_ + path, StringPiece(data, git_blob_rawsize(obj)));
+            } else {
+                fprintf(stderr, "Unable to convert git tree entry %s to object, skipping\n", oid);
+                continue;
+            }
         } else if (git_tree_entry_type(*it) == GIT_OBJ_COMMIT) {
             // Submodule
             if (!walk_submodules_) {